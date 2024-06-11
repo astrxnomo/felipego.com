4ec32f7b82bf105ff99e600003813b84daa44286
astrxnomo <dev@felipego.com>
2024-06-10 20:08:29 -0500

    Add Vercel Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://felipego.com',
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
 });
