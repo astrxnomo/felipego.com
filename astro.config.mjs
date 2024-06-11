3e87a3eb046a95f8049808b4c89c2e4cd95f888c
astrxnomo <dev@felipego.com>
2024-06-10 20:15:32 -0500

    Update Astro config and package dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from 'astro/config';
-
-import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
