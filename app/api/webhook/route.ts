c0b89a441cce632cdb6ec470ed240ac7b1ccd0be
astrxnomo <dev@felipego.com>
2024-12-21 22:53:43 -0500

    refactor webhook route to validate revalidation path header and improve error handling

--- app/api/webhook/route.ts
+++ app/api/webhook/route.ts
@@ -1,34 +1,23 @@
 import { NextResponse } from 'next/server'
 import { revalidatePath } from 'next/cache'
 
-const VERCEL_DEPLOY_HOOK_NOTION_UPDATE = process.env.VERCEL_DEPLOY_HOOK_NOTION_UPDATE
 const NOTION_WEBHOOK_KEY = process.env.NOTION_WEBHOOK_KEY
 
 export async function POST (request: Request) {
   try {
     const notionSecret = request.headers.get('X-Notion-Secret')
+    const pathToRevalidate = request.headers.get('X-Revalidate-Path')
 
     if (!notionSecret || notionSecret !== NOTION_WEBHOOK_KEY) {
       return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
     }
 
-    await request.json()
-    revalidatePath('/')
-
-    if (VERCEL_DEPLOY_HOOK_NOTION_UPDATE) {
-      const response = await fetch(VERCEL_DEPLOY_HOOK_NOTION_UPDATE, {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        }
-      })
-
-      if (!response.ok) {
-        throw new Error('Error triggering deployment on Vercel')
-      }
-    } else {
-      throw new Error('VERCEL_DEPLOY_HOOK_NOTION_UPDATE is not defined')
+    if (!pathToRevalidate) {
+      return NextResponse.json({ error: 'Missing X-Revalidate-Path header' }, { status: 400 })
     }
+
+    await request.json()
+    revalidatePath(pathToRevalidate)
 
     return NextResponse.json({ success: true })
   } catch (error) {
