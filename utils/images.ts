abbac6eecfd8e4588f644520ad3e344aa63392c7
astrxnomo <lgiraldoo@unal.edu.co>
2024-12-24 15:06:10 -0500

    refactor uploadToVercelBlob and processImageUrl functions to improve error handling and logging

--- utils/images.ts
+++ utils/images.ts
@@ -1,16 +1,25 @@
 import { notion } from '@/lib/notion'
 import { put } from '@vercel/blob'
+import fetch from 'node-fetch' // Ensure fetch is available in your environment
 
 export const isNotionUrl = (url: string): boolean => {
-  return url.startsWith('https://prod-files-secure.s3.us-west-2.amazonaws.com') ||
-         url.startsWith('https://s3.us-west-2.amazonaws.com')
+  return (
+    url.startsWith('https://prod-files-secure.s3.us-west-2.amazonaws.com') ||
+    url.startsWith('https://s3.us-west-2.amazonaws.com')
+  )
 }
 
 export async function uploadToVercelBlob (url: string, filename: string): Promise<string> {
   try {
     const response = await fetch(url)
-    const blob = await response.blob()
-    const { url: blobUrl } = await put(filename, blob, { access: 'public' })
+    if (!response.ok) {
+      throw new Error(`Failed to fetch image from URL: ${url}`)
+    }
+    const buffer = await response.buffer()
+    const { url: blobUrl } = await put(filename, buffer, {
+      access: 'public',
+      contentType: 'image/webp' // Adjust the content type as needed
+    })
     return blobUrl
   } catch (error) {
     console.error('Error uploading to Vercel Blob:', error)
@@ -21,24 +30,28 @@ export async function uploadToVercelBlob (url: string, filename: string): Promis
 export async function processImageUrl (url: string, id: string, prefix: string): Promise<string> {
   if (isNotionUrl(url)) {
     const newUrl = await uploadToVercelBlob(url, `${prefix}-${id}.webp`)
-    await notion.pages.update({
-      page_id: id,
-      properties: {
-        img: {
-          files: [
-            {
-              name: `${prefix}-${id}.webp`,
-              external: {
-                url: newUrl
+    try {
+      await notion.pages.update({
+        page_id: id,
+        properties: {
+          img: {
+            files: [
+              {
+                name: `${prefix}-${id}.webp`,
+                external: {
+                  url: newUrl
+                }
               }
-            }
-          ]
+            ]
+          }
         }
-      }
-    })
-    console.log(`Updated image URL for ${id}`)
+      })
+      console.log(`Updated image URL for page ID: ${id}`)
+    } catch (error) {
+      console.error(`Error updating Notion page ID ${id}:`, error)
+    }
     return newUrl
   }
-  console.log(`Image URL for ${id} is a External URL`)
+  console.log(`Image URL for page ID ${id} is an external URL`)
   return url
 }
