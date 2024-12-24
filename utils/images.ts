ed1311c886928db6e87ec2856f6de975aa3cd70c
astrxnomo <lgiraldoo@unal.edu.co>
2024-12-24 15:18:42 -0500

    refactor processImageUrl function to return original URL for non-Notion URLs and streamline logic

--- utils/images.ts
+++ utils/images.ts
@@ -1,12 +1,10 @@
 import { notion } from '@/lib/notion'
 import { put } from '@vercel/blob'
-import fetch from 'node-fetch' // Ensure fetch is available in your environment
+import fetch from 'node-fetch'
 
 export const isNotionUrl = (url: string): boolean => {
-  return (
-    url.startsWith('https://prod-files-secure.s3.us-west-2.amazonaws.com') ||
-    url.startsWith('https://s3.us-west-2.amazonaws.com')
-  )
+  return url.startsWith('https://prod-files-secure.s3.us-west-2.amazonaws.com') ||
+         url.startsWith('https://s3.us-west-2.amazonaws.com')
 }
 
 export async function uploadToVercelBlob (url: string, filename: string): Promise<string> {
@@ -18,40 +16,40 @@ export async function uploadToVercelBlob (url: string, filename: string): Promis
     const buffer = await response.buffer()
     const { url: blobUrl } = await put(filename, buffer, {
       access: 'public',
-      contentType: 'image/webp' // Adjust the content type as needed
+      contentType: 'image/webp'
     })
     return blobUrl
   } catch (error) {
     console.error('Error uploading to Vercel Blob:', error)
-    return url // Return original URL if upload fails
+    return url
   }
 }
 
 export async function processImageUrl (url: string, id: string, prefix: string): Promise<string> {
-  if (isNotionUrl(url)) {
-    const newUrl = await uploadToVercelBlob(url, `${prefix}-${id}.webp`)
-    try {
-      await notion.pages.update({
-        page_id: id,
-        properties: {
-          img: {
-            files: [
-              {
-                name: `${prefix}-${id}.webp`,
-                external: {
-                  url: newUrl
-                }
-              }
-            ]
-          }
-        }
-      })
-      console.log(`Updated image URL: ${newUrl} for page ID: ${id}`)
-    } catch (error) {
-      console.error(`Error updating Notion page ID ${id}:`, error)
-    }
-    return newUrl
+  if (!isNotionUrl(url)) {
+    return url
   }
-  console.log(`Image URL for page ID ${id} is an external URL: ${url}`)
-  return url
+
+  const newUrl = await uploadToVercelBlob(url, `${prefix}-${id}.webp`)
+  try {
+    await notion.pages.update({
+      page_id: id,
+      properties: {
+        img: {
+          files: [
+            {
+              name: `${prefix}-${id}.webp`,
+              external: {
+                url: newUrl
+              }
+            }
+          ]
+        }
+      }
+    })
+    console.log(`Updated image URL: ${newUrl} for page ID: ${id}`)
+  } catch (error) {
+    console.error(`Error updating Notion page ID ${id}:`, error)
+  }
+  return newUrl
 }
